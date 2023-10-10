bf62ad645e6a895b930dbfabf7b0d47e36f7f679
Ardovigus <68460784+Ardovigus@users.noreply.github.com>
2023-10-10 14:54:48 +0700

    Add files via upload

--- Pemecah_Bacaan_Misa.py
+++ Pemecah_Bacaan_Misa.py
@@ -10,6 +10,7 @@ char_idx = 0
 initial_state = True
 terminal_state = False
 selection_state = False
+conjunction_split = False
 
 idx_start = -1
 idx_end = -1
@@ -19,6 +20,8 @@ double_quote = False
 
 terminal_candidate = []
 split_text = []
+
+char_length = 70
 
 while char_idx < len(text):
     if char_idx == len(text) - 1:
@@ -47,38 +50,124 @@ while char_idx < len(text):
         char_idx += 1
 
     if terminal_state:
-        char_match = re.match('''[.;:?!"]''', text[char_idx])
+        if char_idx - idx_start <= char_length:
+            char_match = re.match('''[.,;:?!"]''', text[char_idx])
 
-        if char_match:
-            if ((text[char_idx] == '.' or text[char_idx] == '?' or text[char_idx] == '!') and (text[char_idx + 1] != '"')) or (text[char_idx] == ';' or text[char_idx] == ':'):
-                terminal_candidate.append(char_idx)
+            if char_match:
+                if ((text[char_idx] == '.' or text[char_idx] == '?' or text[char_idx] == '!') and (text[char_idx + 1] != '"')) or (text[char_idx] == ';' or text[char_idx] == ':'):
+                    terminal_candidate.append(char_idx)
 
-                terminal_state = False
-                selection_state = True
-            elif text[char_idx] == '"' and double_quote:
-                terminal_candidate.append(char_idx)
+                    terminal_state = False
+                    selection_state = True
+                elif text[char_idx] == '"' and double_quote:
+                    terminal_candidate.append(char_idx)
 
-                double_quote = False
+                    double_quote = False
 
-                terminal_state = False
-                selection_state = True
+                    terminal_state = False
+                    selection_state = True
+                elif text[char_idx] == ',':
+                    terminal_candidate.append(char_idx)
+        else:
+            terminal_state = False
+            selection_state = True
+            conjunction_split = True
 
         char_idx += 1
 
     if selection_state:
-        idx_end = terminal_candidate[-1]
+        if conjunction_split:
+            current_text = text[idx_start:idx_start + char_length].lower()
 
-        if text[idx_end] == '.' or text[idx_end] == ';' or text[idx_end] == ':':
-            split_text.append(text[idx_start:idx_end])
-        elif text[idx_end] == '?' or text[idx_end] == '!' or text[idx_end] == '"':
-            split_text.append(text[idx_start:idx_end + 1])
+            conjunction_list = ['dan',
+                                'ataukah'
+                                'serta',
+                                'lagipula',
+                                'lagi pula',
+                                'setelah',
+                                'sejak',
+                                'selanjutnya',
+                                'tetapi',
+                                'tapi',
+                                'melainkan',
+                                'sedangkan',
+                                'atau',
+                                'maupun',
+                                'mau pun',
+                                'untuk',
+                                'agar',
+                                'supaya',
+                                'sebab',
+                                'karena',
+                                'sehingga',
+                                'akibatnya',
+                                'lalu',
+                                'kemudian',
+                                'jika',
+                                'kalau',
+                                'apabila',
+                                'walau',
+                                'meski',
+                                'biar',
+                                'seperti',
+                                'bagai',
+                                'sebagai',
+                                'bahkan',
+                                'yaitu',
+                                'yakni',
+                                'kecuali',
+                                'selain']
 
-        selection_state = False
-        initial_state = True
+            conjunction_list_pos = [(re.search(word, current_text)) for word in conjunction_list]
 
-        # char_idx = idx_end + 1
+            for i in range(len(conjunction_list_pos)):
+                if conjunction_list_pos[i]:
+                    # conjunction_list_pos[i] = conjunction_list_pos[i].start()
+                    terminal_candidate.append(conjunction_list_pos[i].start() + idx_start)
+
+            terminal_candidate.sort()
+
+            idx_end = terminal_candidate[-1]
+
+            if text[idx_end] == ',':
+                split_text.append(text[idx_start:idx_end])
+            else:
+                letter_found = False
+
+                while not(letter_found):
+                    if re.match('[a-zA-Z]', text[idx_end]):
+                        split_text.append(text[idx_start:idx_end])
+                        letter_found = True
+
+                    idx_end -= 1
+
+            conjunction_split = False
+            selection_state = False
+            initial_state = True
+
+            char_idx = idx_end + 1
+        else:
+            idx_end = terminal_candidate[-1]
+
+            if text[idx_end] == '.' or text[idx_end] == ';' or text[idx_end] == ':':
+                split_text.append(text[idx_start:idx_end])
+            elif text[idx_end] == '?' or text[idx_end] == '!' or text[idx_end] == '"':
+                split_text.append(text[idx_start:idx_end + 1])
+
+            selection_state = False
+            initial_state = True
+
+            char_idx = idx_end + 1
+            terminal_candidate.clear()
 
 split_text = [(element + '\n' + '(' + passage + ')') for element in split_text]
+
+for i in range(len(split_text)):    
+    if re.match('[a-z]', split_text[i][0]):
+        first_letter = split_text[i][0].upper()
+        sentence = split_text[i][1:]
+
+        split_text[i] = first_letter + sentence
 
 result = '\n\n'.join(split_text)
 
